4e8ec2cd2da14ce322b40b7ebcc88a154ac8ebca
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-06-11 23:57:23 +0200

    feat: main for new matrix struct

--- cmd/main.go
+++ cmd/main.go
@@ -4,29 +4,36 @@ import (
 	"fmt"
 
 	"github.com/m1thrandir225/go-vs-gocuda/internals/native"
-	"github.com/m1thrandir225/go-vs-gocuda/util"
 )
 
 func main() {
 	matrixSize := 512
-	matrixA := util.CreateMatrix(matrixSize)
-	matrixB := util.CreateMatrix(matrixSize)
+	matrixA := native.NewRandomMatrix(matrixSize)
+	matrixB := native.NewRandomMatrix(matrixSize)
 
-	_, err := native.Multiply(matrixA, matrixB)
+	resultNative, err := matrixA.Multiply(matrixB)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	verificationNative := matrixA.VerifyMultiplication(matrixA, matrixB, resultNative)
+	fmt.Printf("Verification native multiplication: %t\n", verificationNative)
+
+	resultParallel, err := matrixA.MultiplyParallel(matrixB)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	_, err = native.MultiplyParallel(matrixA, matrixB)
+	verificationParallel := matrixA.VerifyMultiplication(matrixA, matrixB, resultParallel)
+	fmt.Printf("Verification parallel multiplication: %t\n", verificationParallel)
+
+	resultParallelWorkerPool, err := matrixA.MultiplyParallelWorkerPool(matrixB)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	_, err = native.MultiplyParallelWorkerPool(matrixA, matrixB)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	verificationParallelWorkerPool := matrixA.VerifyMultiplication(matrixA, matrixB, resultParallelWorkerPool)
+	fmt.Printf("Verification worker pool multiplication: %t\n", verificationParallelWorkerPool)
 }
